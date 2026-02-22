bd3d41cb3a497d5e659a5f6607a7f3855a6cca31
Hamilton 108 <hamilton@hamilton.com>
2026-02-22 14:40:21 +0100

    WIP

--- src/main/java/harborview/nordnet/repository/NordnetAdapterV1.java
+++ src/main/java/harborview/nordnet/repository/NordnetAdapterV1.java
@@ -1,5 +1,8 @@
 package harborview.nordnet.repository;
 
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
+import harborview.adapter.RedisAdapter;
 import harborview.nordnet.*;
 import harborview.nordnet.downloader.Downloader;
 import harborview.nordnet.downloader.PageInfo;
@@ -8,24 +11,50 @@ import harborview.nordnet.stockmarket.StockOptionTicker;
 import harborview.nordnet.stockmarket.StockPrice;
 import harborview.nordnet.stockmarket.StockTicker;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
+import vega.financial.StockOptionType;
+import vega.financial.calculator.OptionCalculator;
 
+import java.time.LocalDate;
+import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 
 @Component
 @Primary
 public class NordnetAdapterV1 extends NordnetAdapterBase implements NordnetRepository {
 
-    public NordnetAdapterV1(Downloader<PageInfo> downloader) {
+    private final RedisAdapter redisAdapter;
+    private final OptionCalculator blackScholes;
+    private final boolean fetchOpeningPrice;
+    private final LocalDate curDate;
+    private final Cache<Integer, Tuple2<StockPrice,List<StockOption>>> cacheStockOptions;
+    private final Cache<String, Tuple2<StockPrice,List<StockOption>>> cacheStockOption;
+
+    public NordnetAdapterV1(Downloader<PageInfo> downloader,
+                            RedisAdapter redisAdapter,
+                            @Qualifier("blackScholes") OptionCalculator blackScholes,
+                            @Value("${curdate:#{null}}") String curDateStr,
+                            @Value("${cache.options.expiry}") int optionsExpiry,
+                            @Value("${cache.option.expiry}") int optionExpiry,
+                            @Value("${redis.fetchOpeningPrice}") boolean fetchOpeningPrice) {
         super(downloader);
+        this.redisAdapter = redisAdapter;
+        this.blackScholes = blackScholes;
+        this.fetchOpeningPrice = fetchOpeningPrice;
+
+        curDate = getDateFor(curDateStr,null);
+
+        cacheStockOptions = Caffeine.newBuilder().expireAfterWrite(optionsExpiry, TimeUnit.MINUTES).build();
+        cacheStockOption = Caffeine.newBuilder().expireAfterWrite(optionExpiry, TimeUnit.SECONDS).build();
     }
 
     @Override
     public List<StockOption> getCalls(StockTicker ticker) {
-        System.out.println(downloader);
-        return List.of();
+        return getOptions(ticker, StockOptionType.CALL);
     }
 
     @Override
@@ -46,5 +75,16 @@ public class NordnetAdapterV1 extends NordnetAdapterBase implements NordnetRepos
     @Override
     public void resetCaffeine() {
 
+    }
+
+    private List<StockOption> getOptions(StockTicker ticker, StockOptionType ot) {
+        var result = parse(ticker);
+        if (result == null) {
+            return Collections.emptyList();
+        }
+        return result.second().stream().filter(x -> x.getOpType() == ot).toList();
+    }
+    private Tuple2<StockPrice,List<StockOption>> parse(StockTicker ticker) {
+        return null;
     }
 }
